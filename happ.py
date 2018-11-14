561864a7beca0a84c897bf6e36aa43b0f3fd1aa1
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-11-14 13:35:08 +0000

    add session handling

--- happ.py
+++ happ.py
@@ -1,9 +1,8 @@
-from flask import Flask
-from flask import render_template
-from flask import redirect
+from flask import Flask, render_template, redirect, request, session, escape, url_for
 
 app = Flask(__name__)
 
+app.secret_key = b'dmVyeXZlcnl2ZXJ5c2VjdXJl'
 
 @app.route('/', methods=['GET'])
 @app.route('/<path>', methods=['GET'])
@@ -17,3 +16,25 @@ def hello_world(path=''):
 @app.errorhandler(404)
 def fourohfour(error):
     return '404!', 404
+
+@app.route('/secure', methods=['GET', 'POST'])
+def access_secure_area():
+    if 'username' in session:
+        return '''
+                <h2>Logged in as '{}'</h2>
+                <a href="{}">Logout</a>
+            '''.format(escape(session['username']), url_for('logout'))
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('access_secure_area'))
+    return '''
+        <form method="POST">
+            <p><input type="text" name="username">
+            <p><input type="submit" value="Login">
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('hello_world'))
